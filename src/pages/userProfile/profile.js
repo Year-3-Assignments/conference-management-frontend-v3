e33b3cfea1af1e8e3acb05b7901643e94a81680c
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-05-23 19:56:55 +0530

    Implemented user profile template

--- src/pages/userProfile/profile.js
+++ src/pages/userProfile/profile.js
@@ -1,27 +1,53 @@
 import React from 'react';
 import './userProfilePage.scss';
 
-let initialState = {};
+let initialState = {
+  firstName: '',
+  lastName: '',
+  userName: '',
+  description: '',
+  profileImage: '',
+  isEditClicked: false,
+  buttonText: 'edit profile'
+};
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
+    this.onEditClick = this.onEditClick.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.state = initialState;
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onEditClick(e) {
+    this.setState({ isEditClicked: !this.state.isEditClicked }, () => {
+      this.setState({ buttonText: this.state.isEditClicked ? 'cancel': 'edit profile' })
+    });
   }
 
   render() {
     return (
-      <div className="p-lg-3 profile-card">
-        <div className="card">
-          <div className="row">
-            <div className="col-6">
-              <img
-                src="https://mdbootstrap.com/img/new/standard/nature/111.jpg"
-                className="rounded-circle" width="250"
-              />
-            </div>
-            <div className="col-6">
-              <h2>Rusiru Abhisheak</h2>
+      <div>
+        <div className="">
+          <div className="card">
+            <div>
+              <img src={this.state.profileImage} className="mb-3 profile-img" />
+              <div className="p-2">
+                <h4 className="m-0">{this.state.firstName}&nbsp;{this.state.lastName}</h4>
+                {!this.state.isEditClicked ?
+                  <p className="username mb-1">@{this.state.userName}</p> : 
+                  <input type="text" className="form-control mb-2 mt-2" id="name" name="userName" onChange={this.onChange} value={this.state.userName} />
+                }
+                {!this.state.isEditClicked ?
+                  <p>{this.state.description}</p> :
+                  <textarea className="form-control mb-3" id="description" rows="2" name="description" onChange={this.onChange} value={this.state.description}></textarea>
+                }
+                <button className="btn btn-color btn-block" onClick={this.onEditClick}>{this.state.buttonText}</button>
+              </div>
             </div>
           </div>
         </div>
