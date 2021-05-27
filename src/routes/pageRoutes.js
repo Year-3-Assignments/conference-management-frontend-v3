0bb8d2381abcae36dfdee7b09d3f56e832adb706
RaveenaWickramasinghe <raveenawickramasinghe@gmail.com>
2021-05-27 16:15:34 +0530

    Few changes in page routes

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -7,9 +7,6 @@ import Navbar from '../components/navBar/navbar';
 import Footer from '../components/footer/footer';
 import Login from '../pages/login/login';
 import CreateResource from '../components/resourceCreator/createResource';
-import Profile from '../pages/userProfile/profile';
-import EditProfile from '../pages/userProfile/editProfle';
-import UserProfilePage from '../pages/userProfile/userProfilePage';
 
 class PageRoutes extends React.Component {
   render() {
@@ -21,8 +18,7 @@ class PageRoutes extends React.Component {
             <Switch>
               <Route path="/login" component={Login} exact />
               <Route path="/signup" component={SignUp} exact />
-              <Route path="/" component={CreateResource} exact/>
-              <Route path="/me" component={UserProfilePage} exact />
+              <Route path="/" component={CreateResource}/>
             </Switch>
           </section>
           <Footer/>
