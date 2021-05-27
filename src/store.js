cd8a4d208af87bb8908d6adc5c1e350b4e8a5df0
RaveenaWickramasinghe <68574174+RaveenaWickramasinghe@users.noreply.github.com>
2021-05-27 16:05:34 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -3,10 +3,10 @@ import promise from 'redux-promise-middleware';
 import allReducers from './reducers/index';
 
 // production store
-//const Store = createStore(allReducers, applyMiddleware(promise));
+const Store = createStore(allReducers, applyMiddleware(promise));
 
 // development store. comment this part before you push code to github
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
-const Store = createStore(allReducers, composeEnhancers(applyMiddleware(promise)));
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+//const Store = createStore(allReducers, composeEnhancers(applyMiddleware(promise)));
 
 export default Store;
