5f583ecdb11c0f6201fba0428ac9ba596bc0cbb7
Rusiru Abhisheak <57069956+rusiruavb@users.noreply.github.com>
2021-06-12 23:27:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+import https from 'https';
 
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+setInterval(function() {
+  https.get(process.env.REACT_APP_API_STG_URL);
+  console.log('Root service called');
+}, 3000)
 
 app.listen(port);
 console.log('Server started');
